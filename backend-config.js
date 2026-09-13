eaba9e798dac5b973a6bdf21523b39f2241b3da0
EscobarRKZ <fireman471live@gmail.com>
2026-09-13 23:03:07 +0500

    Configure Turnstile site key

--- backend-config.js
+++ backend-config.js
@@ -1,6 +1,6 @@
 window.MC_BACKEND_V2={
   supabaseUrl:'https://nzcbapjgwqqafmacizqe.supabase.co',
   supabaseAnonKey:'sb_publishable_eDlHZ7U_c7P2xXTmYKM3Jw_-UXzaqAo',
-  turnstileSiteKey:'',
+  turnstileSiteKey:'0x4AAAAAAEy17Ra8LUvZlF67',
   enabled:true
 };
